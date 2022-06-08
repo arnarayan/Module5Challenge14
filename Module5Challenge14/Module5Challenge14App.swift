be36c70589125ec079947e4edf6b29b6024499d5
Anand.Narayan <arnarayan@gmail.com>
2022-06-08 15:14:05 -0600

    Initial Commit

---
+++ Module5Challenge14/Module5Challenge14App.swift
@@ -0,0 +1,17 @@
+//
+//  Module5Challenge14App.swift
+//  Module5Challenge14
+//
+//  Created by Anand Narayan on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct Module5Challenge14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
